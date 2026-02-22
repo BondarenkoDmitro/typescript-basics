8399072839689205fc471ffbaf83f31352a31052
BondarenkoDmitro <kinglion2579@gmail.com>
2026-02-22 09:49:53 +0200

    css clear

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,11 +1,2 @@
-// Імпортуємо все необхідне
 import './new_step';
-import { setupCounter } from './counter';
 
-// Ініціалізуємо приложение при завантаженні DOM
-document.addEventListener('DOMContentLoaded', () => {
-  const counterElement = document.getElementById('counter-value');
-  if (counterElement) {
-    setupCounter(counterElement);
-  }
-});
